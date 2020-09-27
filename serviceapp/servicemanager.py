24de24aabb421377b78a01eed93c737cd0a6ca9e
boitenoire-dev <isaacm90@gmail.com>
2020-09-27 19:46:31 -0400

    Created setup file for iinstallation of script

--- serviceapp/servicemanager.py
+++ serviceapp/servicemanager.py
@@ -16,6 +16,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     '''Service manager manages AWS resources'''
@@ -86,10 +90,17 @@ def create_snapshots(project):
         print("Stopping {0}...".format(i.id))
         i.stop()
         i.wait_until_stopped()
+
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print(" Skipping {0}, snapshot already in progress")
+                continue
+
             print("Creating snapshot og {0}".format(v.id))
             v.create_snapshot(Description="Created by service manager")
+
         print("Starting {0}...".format(i.id))
+
         i.start()
         i.wait_until_running()
 
